219a68dd8d539984ae98fdfe73a00fa0c9728b88
Roberto Esteves <info@robertoesteves.com>
2020-06-06 19:30:29 -0500

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -45,7 +45,7 @@ def clean_data(df):
 
     # clean
     df.drop_duplicates(subset='message', inplace=True)
-    df.dropna(subset=category_colnames, inplace=True)
+    df.dropna(subset=category_colname, inplace=True)
     df.related.replace(2, 0, inplace=True)
 
     return df
